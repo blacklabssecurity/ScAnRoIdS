7106bc63a5a6ef507eeb52ef7ba25ca2f3ca3e33
m4l3n1c3 <blacklabssec@gmail.com>
2026-03-17 20:11:56 -0400

    Create busta.py

---
+++ modules/busta.py
@@ -0,0 +1,11 @@
+gobuster.py setup
+
+General Purpose: /Discovery/Web-Content/raft-medium-directories.txt.
+Quick Wins: /Discovery/Web-Content/common.txt.
+Large-Scale: /Discovery/Web-Content/directory-list-2.3-medium.txt
+
+Syntax for File-Specific Discovery:
+gobuster dir -u http://10.10.10.123 -w /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -t 50 -x php,txt,bak,old
+Success Logic: Appending extensions like .bak or .old with the -x flag often uncovers sensitive source code backups or unpatched legacy scripts.
+Syntax for Virtual Host (VHost) Enumeration:
+gobuster vhost -u http://target.com -w /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-5000.txt --append-domain
