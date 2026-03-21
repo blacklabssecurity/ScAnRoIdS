e42d42de1bf9762a8d86767489b9fbedd79c94e6
m4l3n1c3 <blacklabssec@gmail.com>
2026-03-20 21:36:33 -0400

    Update and rename busta.py to ToDo_busta.py
    Signed-off-by: m4l3n1c3 <blacklabssec@gmail.com>

--- modules/ToDo_busta.py
+++ modules/ToDo_busta.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+"""
+Project: Scanroids Red Team Orchestrator
+Module:  modules/busta.py
+Purpose: Leverages gobuster to enumerate additional web related resources. 
+"""
+
+'''
 gobuster.py setup
 
 General Purpose: /Discovery/Web-Content/raft-medium-directories.txt.
@@ -9,3 +17,4 @@ gobuster dir -u http://10.10.10.123 -w /usr/share/wordlists/dirbuster/directory-
 Success Logic: Appending extensions like .bak or .old with the -x flag often uncovers sensitive source code backups or unpatched legacy scripts.
 Syntax for Virtual Host (VHost) Enumeration:
 gobuster vhost -u http://target.com -w /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-5000.txt --append-domain
+'''
