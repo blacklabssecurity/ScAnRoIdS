bef2e2c940ad71a2ebfcd0eb18f648ff779c308a
m4l3n1c3 <blacklabssec@gmail.com>
2026-03-20 21:39:24 -0400

    Update and rename bloody.py to ToDo_sniffles.py
    Signed-off-by: m4l3n1c3 <blacklabssec@gmail.com>

---
+++ modules/ToDo_sniffles.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Project: Scanroids Red Team Orchestrator
+Module:  modules/sniffles.py
+Purpose: Triggered bloodhound efforts to further enumerate WIndows Domain based environments.
+"""
+
+
