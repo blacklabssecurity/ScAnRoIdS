e2556114777b5eff6652a659c375566dd5c5b525
m4l3n1c3 <blacklabssec@gmail.com>
2026-03-20 22:11:46 -0400

    Update ToDo_sniffles.py
    Signed-off-by: m4l3n1c3 <blacklabssec@gmail.com>

--- modules/ToDo_sniffles.py
+++ modules/ToDo_sniffles.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Project: Scanroids Red Team Orchestrator
+Project: ScAnRoIdS Red Team Orchestrator
 Module:  modules/sniffles.py
 Purpose: Triggered bloodhound efforts to further enumerate WIndows Domain based environments.
 """
