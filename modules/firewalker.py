204cc9f76793fb4873eb9afe351b3721ca671ca4
m4l3n1c3 <blacklabssec@gmail.com>
2026-03-20 22:11:21 -0400

    Update firewalker.py
    Signed-off-by: m4l3n1c3 <blacklabssec@gmail.com>

--- modules/firewalker.py
+++ modules/firewalker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Project: Scanroids Red Team Orchestrator
+Project: ScAnRoIdS Red Team Orchestrator
 Module:  modules/firewalker.py
 Purpose: Tool to stage firewalking efforts against a specified single target.
          Stage 1 gathers the ttl to the destination while stage 2 attempts to 
