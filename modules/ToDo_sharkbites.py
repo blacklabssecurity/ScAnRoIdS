57c85289e52f0b9106f7a5247d592335f1995d04
m4l3n1c3 <blacklabssec@gmail.com>
2026-03-20 21:52:14 -0400

    Create ToDo_sharkbites.py
    Signed-off-by: m4l3n1c3 <blacklabssec@gmail.com>

---
+++ modules/ToDo_sharkbites.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Project: Scanroids Red Team Orchestrator
+Module:  modules/sharkbites.py
+Purpose: Tool will search scans directories and read all pcaps with tshark 
+         and perform automated searches against packet contents for juicy 
+         chunks of information.
+"""
